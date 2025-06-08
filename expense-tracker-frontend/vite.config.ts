fb49acbe3805441a6ef0941e5f08b96d449806b3
Naman Jain <73544325+namanjain27@users.noreply.github.com>
2025-06-09 01:58:23 +0530

    Update vite.config.ts

--- expense-tracker-frontend/vite.config.ts
+++ expense-tracker-frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
+      'https://expense-tracker-frontend-32m0.onrender.com/api': {
         target: 'https://expense-tracker-backend-ayln.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
